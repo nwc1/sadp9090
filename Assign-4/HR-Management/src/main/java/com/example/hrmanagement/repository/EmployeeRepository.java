f8c18f1667707a0de3b9080a1328a396cba36be0
nwc1 <166301633+nwc1@users.noreply.github.com>
2024-12-13 03:19:23 +0530

    Delete Assign-4 directory

--- Assign-4/HR-Management/src/main/java/com/example/hrmanagement/repository/EmployeeRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.hrmanagement.repository;
-
-import com.example.hrmanagement.model.Employee;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface EmployeeRepository extends JpaRepository<Employee, Long> {
-}
